40716d64e3052d3a47a19d1035a3012e3e5f705f
Lê Hoàng Giáp <85095539+lehoanggiap@users.noreply.github.com>
2024-04-21 14:12:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function (req, res) {
   console.log("Ip address", req.socket.remoteAddress)
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(8080);
+console.log('Server running');
